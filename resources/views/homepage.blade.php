aca3ca3276c7765f4fc459c22eb04f7b6106ab6f
Simone Marino <simo.marino@outlook.it>
2023-03-29 22:12:52 +0200

    Ex Done Commit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.app')
+
+@section('page_name')
+Homepage
+@endsection
+ 
+@section('main_content')
+ <section class="container"> 
+  <div class="row row-cols-4">
+     @foreach ($movies as $movie)
+    <div class="thumbs img-fluid d-flex flex-column">
+      <div class="card mt-auto" style="width: 18rem;">
+        {{-- <img src="<img class=" src="{{ Vite::asset('resources/img/godFather.jpg') }}" class="card-img-top" alt="..."> --}}
+        <div class="card-body">
+          <h5 class="card-title">{{ strtoupper($movie->title) }}</h5>
+          <p class="card-text">{{ $movie->nationality }} - {{ $movie->original_title }}</p>
+          <p class="card-text">{{ $movie->date }}</p>
+          <p class="card-text">Average vote: {{ $movie->vote }}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    @endforeach
+  </div>
+ </section>
+@endsection
+
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,12 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>{{ env('APP_NAME')}}</title>
-
+  <title>{{ env('APP_NAME')}} - @yield('page_name')</title>
   {{-- Includiamo gli assets con la direttiva @vite --}}
   @vite('resources/js/app.js')
 </head>
+
 <body>
-  
+  <main>
+    @yield('main_content')
+  </main>
 </body>
 </html>
