7fd9941a1cd0987e735e024c02ffde5d36ec78d4
Simone Marino <simo.marino@outlook.it>
2023-03-29 15:18:28 +0200

    Config env + Model + Controller

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Laravel Model Controller</title>
+  <title>{{ env('APP_NAME')}}</title>
 
   {{-- Includiamo gli assets con la direttiva @vite --}}
   @vite('resources/js/app.js')
